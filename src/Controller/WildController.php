075b709621326a92bf0d8b326fc95f3f76f6df2c
Philippe Urban <spartitus@hotmail.fr>
2019-11-18 19:07:03 +0100

    quête 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class WildController extends AbstractController
 {
@@ -17,4 +17,25 @@ class WildController extends AbstractController
             'website' => 'Wild series',
         ]);
     }
+
+    /**
+     * @Route("/wild/show/{slug}",
+     *      requirements={"slug"="^[a-z0-9]+(?:-[a-z0-9]+)*$"},
+     *      name="wild_show")
+     *      @param string $slug
+     *      @return Response
+     */
+    public function show(string $slug =''): Response
+    {
+        if ($slug === ''){
+            $title = 'Aucune série selectionnée, veuillez choisir une série';
+        } else {
+            $title = ucwords(str_replace('-', '', $slug));
+        }
+
+
+        return $this->render('wild/show.html.twig', ['title' => $title]);
+    }
+
+
 }
