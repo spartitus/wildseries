4944a6f5b2e00cd3468c7a3bd36e694f14ccf772
philippe <spartitus>
2020-01-13 13:44:37 +0100

    Quete 16 correction bug

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -7,7 +7,7 @@ class Slugify
     {
         $text = preg_replace('~[^\pL\d]+~u', '-', $input);
         $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-        //$text = preg_replace('~[^-\w]+~', '', $text);
+        $text = preg_replace('~[^-\w]+~', '', $text);
         $text = trim($text, '-');
         $text = preg_replace('~-+~', '-', $text);
         $text = strtolower($text);
